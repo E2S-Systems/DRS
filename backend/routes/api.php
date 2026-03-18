8b1cd2b9e26fa29f83d01cf6e3eaffa88da8d54f
MachadoEduardo <05eduardomachado@gmail.com>
2026-03-18 20:08:42 -0300

    fix: CSRF token and route adjustments for auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 
 Route::prefix('v1')->group(function () {
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('login', 'login')->name('login');
+        Route::post('logout', 'logout')->middleware('auth:sanctum')->name('logout');
+    });
+
     Route::middleware('auth:sanctum')->group(function () {
         Route::apiResource('users', UserController::class);
     });
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,10 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-
-
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login')->name('login');
-    Route::post('logout', 'logout')->middleware('auth:sanctum')->name('logout');
-});
