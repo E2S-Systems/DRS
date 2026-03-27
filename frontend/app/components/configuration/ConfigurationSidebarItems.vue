0c7265a43b6f5fa2968614b0f34908447cb2b3f0
MachadoEduardo <05eduardomachado@gmail.com>
2026-03-27 20:25:34 -0300

    feat: configuration sidebar with dinamyc path subtitle

---
+++ frontend/app/components/configuration/ConfigurationSidebarItems.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtLink
+    :to="to"
+    class="text-text-muted hover:text-white text-md px-4 py-2"
+    active-class="bg-bg-panel text-white border-l-2 border-primary"
+  >
+    <i :class="`pi pi-${icon}`"/>
+    <span class="px-3">{{ label }}</span>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  label: string
+  icon: string
+  to: string
+}>()
+
+</script>
---
+++ frontend/app/components/configuration/ConfigurationSidebarSection.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps<{ label: string }>()
+</script>
+
+<template>
+  <div class="">
+    <span class="text-text-muted tracking-wide m-2">{{ label }}</span>
+    <slot />
+  </div>
+</template>
