490082eac3f768abe6df5c57b22099648ce54806
MachadoEduardo <05eduardomachado@gmail.com>
2026-03-28 01:11:06 -0300

    feat: useUsers composable with interfaces (Paginated, for JSON response and User for response.data) and using nuxt-sanctum lib

--- frontend/app/Composables/useUsers.ts
+++ frontend/app/Composables/useUsers.ts
@@ -1,3 +1,21 @@
+interface PaginatedResponse<T> {
+  data: T[]
+  links: {
+    first: string
+    last: string
+    prev: string | null
+    next: string | null
+  }
+  meta: {
+    current_page: number
+    last_page: number
+    per_page: number
+    total: number
+  }
+  success: boolean
+  message: string
+}
+
 interface User {
   id: number
   first_name: string
@@ -5,17 +23,22 @@ interface User {
   email: string
   role: string
   is_active: boolean
-  last_login_at: Date
+  last_login_at: string
 }
 
 export function useUsers() {
   const client = useSanctumClient()
   const { public: { apiUrl } } = useRuntimeConfig()
 
-  const { data, pending, error, refresh } = useFetch<User[]>(`${apiUrl}/users`, {
-    $fetch: client,
-    default: () => [],
-  })
+  const { data: response, pending, error, refresh } = useFetch<PaginatedResponse<User>>(
+    `${apiUrl}users`,
+    {
+      $fetch: client,
+      default: () => null,
+    }
+  )
+
+  const data = computed(() => response.value?.data ?? [])
 
   return { data, pending, error, refresh }
 }
